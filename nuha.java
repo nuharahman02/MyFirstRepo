c773eb65a640f878c4d8b92007ae360918e9f2ad
nuharahman02 <115097649+nuharahman02@users.noreply.github.com>
2022-10-05 21:42:07 +0600

    Add files via upload

---
+++ nuha.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class nuha {
+    public static void main(String[] args){
+        String name;
+        Scanner n=new Scanner(System.in);
+        System.out.println("Type your nane: ");
+        name=n.nextLine();
+        //int y = 6;
+        System.out.println(name.toUpperCase());
+    }
+}
